9ec20c1700d6e1b19708a17ffb515b95cad28d7f
AlenJohnSunny <alanjohnsunny@gmail.com>
2022-05-06 15:18:06 +0530

    First Commit

---
+++ src/test/java/com/coolascode/app/PropelDec2021CoolAsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolascode.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropelDec2021CoolAsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
